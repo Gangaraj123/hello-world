ccd097462304b397d3fd4b67e3b1e43ef6ec0e40
RAJU <gangarajbopparam@gmail.com>
2022-07-29 14:40:35 +0530

    23

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const RANDOM_QUOTE_API_URL = 'http://api.quotable.io/random'
+const RANDOM_QUOTE_API_URL = 'https://api.quotable.io/random'
 let flag = true
 const quote_display_element = document.getElementById('quote-display')
 const quote_input_element = document.getElementById('quoteInput')
